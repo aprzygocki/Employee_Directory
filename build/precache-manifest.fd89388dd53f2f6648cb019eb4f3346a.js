914f3cf8560891451285688acec07342076bb009
aprzygocki <augustino.przygocki@yahoo.com>
2020-12-28 23:54:28 -0800

    package.json url

---
+++ build/precache-manifest.fd89388dd53f2f6648cb019eb4f3346a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45faee3f63d912bebc90afacf514d5a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/my-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "bd4da044069f2a34a8ab",
+    "url": "/my-app/static/css/main.a4cd29b7.chunk.css"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/my-app/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "bd4da044069f2a34a8ab",
+    "url": "/my-app/static/js/main.3c3cb9c1.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
